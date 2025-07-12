991ee3543e24bdfcff1ebd21d76452869a0e34cb
AnthonyLS32 <anthonylasala@hotmail.com>
2025-07-12 10:38:19 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import datetime
 st.set_page_config(page_title="AFL Replay Downloader")
 st.title("AFL Replay Downloader")
 
-# Date picker and Download button
+# Pick date
 selected_date = st.date_input("Select date to download", datetime.date.today())
 
 if st.button("Download Replays"):
@@ -19,16 +19,11 @@ if st.button("Download Replays"):
     result = subprocess.run(cmd, capture_output=True, text=True)
     st.text_area("Logs", result.stdout + result.stderr, height=300)
 
-# List downloaded files for direct download
+# Show downloaded files
 dl_dir = "./AFL_Replays"
 if os.path.isdir(dl_dir):
     st.subheader("Downloaded Replays")
     for fname in sorted(os.listdir(dl_dir)):
         path = os.path.join(dl_dir, fname)
         with open(path, "rb") as fp:
-            st.download_button(
-                label=fname,
-                data=fp.read(),
-                file_name=fname,
-                mime="video/mp4"
-            )
+            st.download_button(label=fname, data=fp.read(), file_name=fname, mime="video/mp4")
